171df9b6c7d5cbf8a1a51f291c3214f49ce2eac0
ponurakk <p0unrakg@int.pl>
2024-07-17 23:09:41 +0200

    feat(backend): Add message handling

--- src/pmessage_backend/src/lib.rs
+++ src/pmessage_backend/src/lib.rs
@@ -1,4 +1,5 @@
 use candid::{CandidType, Deserialize, Principal};
+// use ic_cdk::trap;
 use std::{cell::RefCell, collections::HashMap};
 
 // TODO: Add Image type
@@ -26,31 +27,95 @@ thread_local! {
 
 #[ic_cdk::update]
 fn send_message(receiver: Principal, content: String) {
-    todo!()
+    let sender = ic_cdk::caller();
+    let timestamp = ic_cdk::api::time() / 1_000_000;
+
+    // TODO: Add proper id selection
+    let message = Message {
+        id: 0,
+        sender,
+        receiver,
+        content,
+        timestamp,
+    };
+
+    STATE.with_borrow_mut(|state| {
+        state
+            .conversations
+            .entry((sender, receiver))
+            .or_default()
+            .push(message);
+    })
 }
 
 #[ic_cdk::query]
 fn get_messages_with(user: Principal) -> Conversation {
-    todo!()
+    let caller = ic_cdk::caller();
+    STATE.with_borrow(|state| {
+        state.conversations.get(&(caller, user)).cloned().unwrap_or(
+            state
+                .conversations
+                .get(&(user, caller))
+                .cloned()
+                .unwrap_or_default(),
+        )
+    })
 }
 
+// TODO: Add proper error handling
 #[ic_cdk::update]
 fn remove_message(receiver: Principal, id: u64) {
-    todo!()
+    let caller = ic_cdk::caller();
+    STATE.with_borrow_mut(|state| {
+        if let Some(conversation) = state.conversations.get_mut(&(caller, receiver)) {
+            let index = conversation.iter().position(|v| v.id == id).unwrap();
+            conversation.remove(index);
+            // trap(r#"{"message": "Message not found"}"#);
+        }
+    })
 }
 
+// TODO: Add proper error handling
 #[ic_cdk::update]
 fn update_message(receiver: Principal, id: u64, new_message: String) {
-    todo!()
+    let caller = ic_cdk::caller();
+    STATE.with_borrow_mut(|state| {
+        if let Some(conversation) = state.conversations.get_mut(&(caller, receiver)) {
+            let index = conversation.iter().position(|v| v.id == id).unwrap();
+            if let Some(v) = conversation.get_mut(index) {
+                v.content = new_message;
+            }
+            // trap(r#"{"message": "Message not found"}"#);
+        }
+    })
+}
+
+// NOTE: Store conversations on stable storage
+#[ic_cdk::pre_upgrade]
+fn pre_upgrade() {
+    STATE.with_borrow(|state| {
+        ic_cdk::storage::stable_save((state.conversations.clone(),)).unwrap();
+    });
+}
+
+// NOTE: Get conversations from stable storage
+#[ic_cdk::post_upgrade]
+fn post_upgrade() {
+    STATE.with_borrow_mut(|state| {
+        if let Ok((conversations,)) =
+            ic_cdk::storage::stable_restore::<(HashMap<(Principal, Principal), Conversation>,)>()
+        {
+            state.conversations = conversations;
+        }
+    });
 }
 
 // NOTE: Remove in prod
 #[ic_cdk::query]
 fn get_all() -> HashMap<(Principal, Principal), Vec<Message>> {
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let caller = ic_cdk::caller();
+    ic_cdk::println!("{:#?}", caller);
+    STATE.with_borrow(|state| {
+        return state.conversations.clone();
+    })
 }
