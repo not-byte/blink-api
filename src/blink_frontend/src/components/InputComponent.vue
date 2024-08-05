6698ec25bca42f93811c60d38b371ae68c23ed21
ponurakk <p0unrakg@int.pl>
2024-08-05 14:16:15 +0200

    fix: Re-Add missing v-model

--- src/blink_frontend/src/components/InputComponent.vue
+++ src/blink_frontend/src/components/InputComponent.vue
@@ -29,7 +29,7 @@ async function send() {
   <aside
     class="w-full h-fit px-9 py-6 lg:px-5 lg:py-3 mt-2 lg:mt-0 mb-0 lg:mb-4 bg-smoke/5 backdrop-blur-sm rounded-2xl border-2 lg:border border-smoke/10 flex">
     <input class="text-[2rem] lg:text-base w-full bg-transparent outline-none placeholder:text-white"
-      placeholder="Enter your message" type="text" />
+      placeholder="Enter your message" type="text" v-model="message" />
     <button @click="send" class="text-[2rem]">→</button>
   </aside>
 </template>
